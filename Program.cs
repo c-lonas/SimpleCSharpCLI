faa36891cc9c33085ce1aa97ef7837941c4629f7
Chase Lonas <chaselonas@gmail.com>
2024-07-24 19:40:52 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            if (args.Length > 0)
+            {
+                Console.WriteLine("Hello, " + String.Join(" ", args) + "!");
+            }
+            else
+            {
+                Console.WriteLine("Hello, World!");
+            }
+        }
+    }
+}
